a069b003c9a30d75e62b2d072796b2e9db88cf0c
Mahi-8686 <mohini.geekologix@gmail.com>
2025-07-10 18:37:34 +0530

    bugs fixes popup

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "018c255a8a3c2a829ad3771fe75125ee",
+const RESOURCES = {"flutter_bootstrap.js": "273f41c7dbbc7aeb0ec049a5a037ef63",
 "version.json": "6689304f4936d470c478e5204dff4c99",
-"index.html": "f6d902c431514edd5a1ab0b1010a421e",
-"/": "f6d902c431514edd5a1ab0b1010a421e",
+"index.html": "cc052024e8f6add1ea0ce8003218fcba",
+"/": "cc052024e8f6add1ea0ce8003218fcba",
 "firebase-messaging-sw.js": "ea8c3345b1b1414c0a25aeea870d2be8",
-"main.dart.js": "ebb7a70afab3f76ad2b4146dc04326a4",
+"main.dart.js": "a9e19500ccff6f2e47c63e587b8ca5ae",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "d1fe45cdb03b853442b38a97d9795724",
 "logo.png": "af8dac01706893566e70ec42b8f2845a",
